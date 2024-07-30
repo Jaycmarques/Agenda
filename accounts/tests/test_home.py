40e4b58fba289d1c1106f68e12d760375f0875b3
Jaycmarques <julio.jcmarques@gmail.com>
2024-07-29 23:36:17 -0300

    criado modulo django_assertions para usar os TestCase assert_contains, configurado teste de botão login.

--- accounts/tests/test_home.py
+++ accounts/tests/test_home.py
@@ -1,8 +1,19 @@
 import pytest
 from django.urls import reverse
+from project.django_assertions import assert_contains
 
 
-@pytest.mark.django_db
+@pytest.fixture
+def resp(client, db):
+    resp = client.get(reverse('home'))
+    return resp
+
+
 def test_home_page_status_code(client):
     response = client.get(reverse('home'))
     assert response.status_code == 200
+
+
+def test_login_link(resp):
+    login_url = reverse('login')
+    assert_contains(resp, f'href="{login_url}"')
