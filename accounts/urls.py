70212d2025ee3e7cec63d3814c5e8f286a8bfaa2
Jaycmarques <julio.jcmarques@gmail.com>
2024-07-29 22:41:52 -0300

    criação de urls, views e templates para registrar, logar e resetar password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import register, home
+from .views import register, home, success, log_in
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
     path('register/', register, name='register'),
+    path('success/', success, name='success'),
+    path('login/', log_in, name='login'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,19 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('/success')
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'accounts/success.html')
+
+
+def log_in(request):
+    return render(request, 'accounts/login.html')
+
+
+def password_reset(request):
+    return render(request, 'accounts/password_reset.html')
