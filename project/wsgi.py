08827773fcef8da1c9299686abd5071f97f832cb
Jaycmarques <julio.jcmarques@gmail.com>
2024-09-16 23:13:16 -0300

    files update

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
 from django.core.wsgi import get_wsgi_application
-
-# Adicione o caminho do projeto ao sys.path
-sys.path.append('/home/jcmarques/jcmarques.pythonanywhere.com/')
 
 # Configure o ambiente Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
