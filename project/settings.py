16037c99e1190cd5aa4c58eb9f7dcb842e78e061
Jaycmarques <julio.jcmarques@gmail.com>
2024-09-19 17:54:04 -0300

    adding allowed host

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '107.178.216.17',
                  ]
 
 AUTH_USER_MODEL = 'accounts.User'
