8074c654abfc5a537af839dae40c96f1e06f586c
Julio Marques <julio.jcmarques@gmail.com>
2024-10-27 22:13:51 -0300

    implementing tests

--- accounts/tests/test_users.py
+++ accounts/tests/test_users.py
@@ -8,8 +8,8 @@ def test_user_registration(client):
     response = client.post(reverse('register'), {
         'email': 'testuser@example.com',  # Usando 'email' em vez de 'username'
         'first_name': 'Test',             # Incluindo o campo 'first_name' se necessário
-        'password1': 'securepassword123',
-        'password2': 'securepassword123'
+        'password1': 'securepassword123!',
+        'password2': 'securepassword123!'
     })
     # Espera-se redirecionamento após registro bem-sucedido
     assert response.status_code == 302
--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,18 +1,50 @@
+import pytest
 from django.test import TestCase
 from django.urls import reverse, resolve
 from accounts import views
+from django.contrib.auth import get_user_model
+from faker import Faker
 
 
 class AccountsViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self):
+    def test_home_view_function_is_correct(self):
         view = resolve(reverse('accounts:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipe_search_view_function_is_correct(self):
+    def test_search_view_function_is_correct(self):
         view = resolve(reverse('accounts:search'))
         self.assertIs(view.func, views.search)
 
-    def test_recipe_contact_view_function_is_correct(self):
+    def test_contact_view_function_is_correct(self):
         view = resolve(
             reverse('accounts:contact', kwargs={'contact_id': 1}))
         self.assertIs(view.func, views.contact)
+
+
+@pytest.mark.django_db
+def test_index_view(client):
+    response = client.get(reverse('index'))
+    assert response.status_code == 200
+    assert 'Welcome' in response.content.decode()
+
+
+fake = Faker()
+
+
+@pytest.mark.django_db
+def test_register_view(client):
+    # Generate a unique email
+    email = fake.email()
+    response = client.post(reverse('register'), data={
+        'email': email,  # Use the generated unique email
+        'first_name': fake.first_name(),
+        'password1': 'newpassword123!',
+        'password2': 'newpassword123!',
+    })
+
+    assert response.status_code == 302
+
+    # Verify that a user with the given email was created
+    User = get_user_model()
+    # Check if the user exists
+    assert User.objects.filter(email=email).exists()
