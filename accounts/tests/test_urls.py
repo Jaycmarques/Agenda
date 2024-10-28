698c7798180a6d8db90f76ff19a8e85ac4b31649
Julio Marques <julio.jcmarques@gmail.com>
2024-10-28 19:34:26 -0300

    including url tests

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -14,3 +14,23 @@ class AccountsURLsTest(TestCase):
     def test_url_contact_detail_ok(self):
         url = reverse('contact', args=(1,))
         self.assertEqual(url, '/contact/1/detail/')
+
+    def test_url_search_ok(self):
+        url = reverse('search')
+        self.assertEqual(url, '/search/')
+
+    def test_url_register_ok(self):
+        url = reverse('register')
+        self.assertEqual(url, '/register/')
+
+    def test_url_success_ok(self):
+        url = reverse('success')
+        self.assertEqual(url, '/success/')
+
+    def test_url_login_ok(self):
+        url = reverse('login')
+        self.assertEqual(url, '/login/')
+
+    def test_url_logout_ok(self):
+        url = reverse('logout')
+        self.assertEqual(url, '/logout/')
